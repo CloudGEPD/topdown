d1e9d574431dd3bafa5eac234809c3eaec0a2536
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-04-23 14:11:14 -0500

    Projectile
    bulletts fly and are destroyed

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float destroytime;
+
+    void Start()
+    {
+        Destroy(gameObject, destroytime);
+    }
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+	
+	
+}
