01439276ae2203848415b102e00580e451165874
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-04-30 13:17:13 -0500

    Gun, character model
    Added in animation skill trees, with character models, crosshair crusor,  and  more

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     public LayerMask groundlayer;
+    public Animator animator;
+
     private float x;
     private float y;
     private Vector3 direction;
@@ -21,7 +23,20 @@ public class Movement : MonoBehaviour
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", x);
+        animator.SetFloat("Vertical", y);
+
         direction = new Vector3(x, 0, y);
+
+        if(direction == Vector3.zero)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", true);
+        }
         direction = Vector3.ClampMagnitude(direction, 1);
         direction *= speed * Time.deltaTime;
         transform.position += direction;
