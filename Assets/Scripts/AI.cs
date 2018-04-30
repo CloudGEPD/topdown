c108db81c94cfaae3dd3ad1ee91d3f7956652216
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-04-30 11:41:18 -0500

    Added Navigation and spawn manager
    Added level design, spawn manager, and navigation for the enemies

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject player;
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+    {
+        agent.destination = player.transform.position;
+	}
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform[] spawns;
+    public int increase_spawn_count;
+    public float wave_length;
+
+    private int wavecount;
+    int spawn_count;
+    bool is_wave_active;
+
+
+    private void Update()
+    {
+        if(!is_wave_active)
+        {
+            is_wave_active = true;
+            StartCoroutine(WaveCount());
+          
+        }
+    }
+
+    IEnumerator WaveCount()
+    {
+        wavecount++;
+        spawn_count += increase_spawn_count;
+
+        for (int i = 0; i < spawn_count; i++)
+        {
+            Transform spawnPosition = spawns[Random.Range(0, spawns.Length)];
+            Instantiate(enemy, spawnPosition.position, spawnPosition.rotation);
+        }
+
+        yield return new WaitForSeconds(wave_length);
+        is_wave_active = false;
+    }
+}
